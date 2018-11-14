726956e32197c465a5e2ea92ebe3d74972db91d1
Xi Gong <xi@ucsb.edu>
2018-11-13 20:22:05 -0800

    Final Version

--- starT.cpp
+++ starT.cpp
@@ -95,18 +95,21 @@ void assertEquals(string expected, string actual, string message="") {
 
 int main(int argc, char *argv[])
 {
+     if (argc!=3) {
+     cerr << "Usage: " << argv[0] << " width height" << endl;
+     exit(1);
+   }
 
-  // TODO: Add check for parameters
-  // and code to print usage message
+   int width = stoi(argv[1]);
+   int height = stoi(argv[2]);
+   // If the program is executed with parameters -1 -1 unit test
+   // the starL() function using our automated test framework
+   if (width==-1 && height==-1) {
+     runTests();
+     exit(0);
+   }
 
-  // TODO: Add code to get width and height from command line args
-  // code that checks if they are both -1; if so, call runTests()
-  // then exit.
-
-  runTests();
-
-  // TODO: Add code that calls the starT function and prints
-  // the result on cout (without an extra newline)
-
+   cout << starT(width,height);
+   return 0; 
   return 0;
 }
